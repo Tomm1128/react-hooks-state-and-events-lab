d8bd257ab62cdce9e93a108bbe6858a1172ce788
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-10 13:51:13 -0400

    Create add to cart functionality

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setCart] = useState(false)
+
+  const updateCart = (event) => {
+    if (event.target.parentElement.childNodes[0].textContent === name){
+      setCart(!inCart)
+    }
+  }
+
+  const btnText = inCart ? "Remove From Cart" : "Add to Cart"
+  const checkCart = inCart ? "in-cart" : ""
+
   return (
-    <li className="">
+    <li className={checkCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={updateCart} className="add">{btnText}</button>
     </li>
   );
 }
